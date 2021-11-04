d6f300a4d0cb8da7af53b2ec57fb6e98edf8c550
KaichiOhnuma <kaichi1026@gmail.com>
2021-11-05 01:31:51 +0900

    merge test

--- ai/git_test.py
+++ ai/git_test.py
@@ -1,1 +1,13 @@
 print('hello world')
+
+print('test')
+def naiseki(a, b):
+    sum = 0
+    for i in range(len(a)):
+            sum += a[i] * b[i]
+
+    return sum
+
+a = [2,3,4,4]
+b = [5,2,2,7]
+print(naiseki(a, b))
