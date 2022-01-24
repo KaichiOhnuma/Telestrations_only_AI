cf0260c480ac422d8c9f05439626b465e7e43efe
KaichiOhnuma <kaichi1026@gmail.com>
2022-01-24 16:43:00 +0900

    enable output to csv file

--- ai/output_test.py
+++ ai/output_test.py
@@ -1,27 +1,73 @@
-import  sys
-from attrs import setters
 import numpy as np
+import pandas as pd
 
-def start():
-    x = int(input("x"))
-    y = int(input("y"))
-    return [x, y]
+class Output_data(object):
+    def __init__(self, file_name):
+        self.file_name = file_name
+        self.load_file()
+        self.output_csv()
 
-def load_file():
-    results_file = np.load("result_data1.npz")
-    avg_sim_to_p_wrd_list = results_file["avg_sim_to_p_wrd_list"]
-    avg_sim_to_s_wrd_list = results_file["avg_sim_to_s_wrd_list"]
-    avg_final_sim_to_s_wrd_list = results_file["avg_final_sim_to_s_wrd_list"]
-    prob_of_failure_list = results_file["prob_of_failure_list"]
-    avg_num_of_wrd_list = results_file["avg_num_of_wrd_list"]
-    setting_list = results_file["setting_list"]
+    def load_file(self):
+        self.results_file = np.load(self.file_name)
+        self.avg_sim_to_p_wrd_list = self.results_file["avg_sim_to_p_wrd_list"]
+        self.avg_sim_to_s_wrd_list = self.results_file["avg_sim_to_s_wrd_list"]
+        self.avg_final_sim_to_s_wrd_list = self.results_file["avg_final_sim_to_s_wrd_list"]
+        self.prob_of_failure_list = self.results_file["prob_of_failure_list"]
+        self.avg_num_of_wrd_list = self.results_file["avg_num_of_wrd_list"]
+        self.setting_list = self.results_file["setting_list"]
 
-    print(avg_sim_to_p_wrd_list)
-    print(avg_sim_to_s_wrd_list)
-    print(avg_final_sim_to_s_wrd_list)
-    print(prob_of_failure_list)
-    print(avg_num_of_wrd_list)
-    print(setting_list)
+        self.truncation_list = []
+        self.mutation_degree_list = []
+        self.mutation_rate_list = []
+
+        for setting in self.setting_list:
+            if not setting[0] in self.truncation_list:
+                self.truncation_list.append(setting[0])
+            if not setting[1] in self.mutation_degree_list:
+                self.mutation_degree_list.append(setting[1])
+            if not setting[2] in self.mutation_rate_list:
+                self.mutation_rate_list.append(setting[2])
+
+    def output_csv(self):
+        self.make_csv_file()
+
+        for mutation_rate in self.mutation_rate_list:
+            for i in range(5):
+                if i == 0:
+                    data_list = self.avg_sim_to_p_wrd_list
+                    data_list_name = "avg_sim_to_p_wrd"
+                elif i == 1:
+                    data_list = self.avg_sim_to_s_wrd_list
+                    data_list_name = "avg_sim_to_s_wrd"
+                elif i == 2:
+                    data_list = self.avg_final_sim_to_s_wrd_list
+                    data_list_name = "avg_final_sim_to_s_wrd"
+                elif i == 3:
+                    data_list = self.prob_of_failure_list
+                    data_list_name = "prob_of_failure_list"
+                elif i == 4:
+                    data_list = self.avg_num_of_wrd_list
+                    data_list_name = "avg_num_of_wrd_list" 
+                data_list_name += ", mutation rate: " + str(mutation_rate)
+                self.add_data_csv(data_list, data_list_name)
+
+    def make_csv_file(self):
+        with open('../data/' + self.file_name.replace(".npz", ".csv"), "w+") as f:
+            pass
+
+    def add_data_csv(self, data_list, data_list_name):
+        output_data = [[truncation] for truncation in self.truncation_list]
+
+        for i, data in enumerate(data_list):
+            case = i // len(self.mutation_degree_list)
+            output_data[case].append(data)
+
+        columns = [data_list_name]
+        columns.extend(self.mutation_degree_list)
+        df = pd.DataFrame(output_data, columns=columns)
+
+        df.to_csv("../data/result_data1.csv", mode='a', index=False)
+        print(df)
 
 if __name__ == "__main__":
-    load_file()
+    output = Output_data("result_data1.npz")
