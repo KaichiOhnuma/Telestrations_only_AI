0b6e5e99dc757157292dbbf27a9c02de8c8a56e6
KaichiOhnuma <kaichi1026@gmail.com>
2022-09-20 16:59:06 +0900

    executed experiment

--- ai/analyzer.py
+++ ai/analyzer.py
@@ -1,10 +1,13 @@
 from re import S
+from sre_constants import SUCCESS
 from turtle import color
 import numpy as np
 import itertools
 import os
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
+import pandas as pd
+import seaborn as sns
 
 class Analyzer(object):
     def __init__(self, truncations, noise_degrees):
@@ -31,7 +34,7 @@ class Analyzer(object):
         step = [i for i in range(len(data))]
         # self.ax1.plot(step, data, label=f"pic:{truncation}, wrd:{noise_degree}", color=cm.jet(truncation/self.truncations[-1]))
         # self.ax1.plot(step, data, label=f"pic:{truncation}, wrd:{noise_degree}", color=cm.jet(noise_degree/self.noise_degrees[-1]))
-        self.ax1.plot(step, data, label=f"pic:{truncation}, wrd:{noise_degree}")
+        self.ax1.plot(step, data, label=f"img:{truncation}, wrd:{noise_degree}")
     
     def analysis_manage(self, data_path):
         for truncation, noise_degree in itertools.product(self.truncations, self.noise_degrees):
@@ -40,6 +43,56 @@ class Analyzer(object):
             self.plot_data(case_res, truncation, noise_degree)
         plt.legend(bbox_to_anchor=(1, 1), loc="upper left", borderaxespad=0, fontsize=10)
         plt.show()
+
+    def make_heatmap(self, data_path):
+        df = []
+
+        for truncation, noise_degree in itertools.product(self.truncations, self.noise_degrees):
+            data = self.load_data(data_path, truncation, noise_degree)
+            success_rate, single_sim, final_sim, sim_ratio = self.make_heatmap_data(data)
+            case_df = [truncation, noise_degree, success_rate, single_sim, final_sim, sim_ratio]
+            df.append(case_df)
+        
+        df_columns = ["img diversity", "wrd diversity", "success rate", "single sim", "final sim", "sim ration"]
+        df = pd.DataFrame(df, columns=df_columns)
+
+        heatmaps = [
+            df.pivot(df_columns[0], df_columns[1], df_columns[2]), 
+            df.pivot(df_columns[0], df_columns[1], df_columns[3]), 
+            df.pivot(df_columns[0], df_columns[1], df_columns[4]), 
+            df.pivot(df_columns[0], df_columns[1], df_columns[5])
+        ]
+        
+        for i, hm in enumerate(heatmaps):
+            plt.figure(figsize=(7,4))
+            heatmap = sns.heatmap(hm, annot=True, cmap="coolwarm", fmt="1.2f")
+            output_path = os.path.join(data_path, str(i)+".png")
+            plt.savefig(output_path)
+
+
+
+
+    def make_heatmap_data(self, data):
+        iteration, step_num = data.shape
+        success_rate, single_sim, final_sim = 0, 0, 0
+
+        for section_data in data:
+            final_sim += self.calc_cos_sim(section_data[0], section_data[-1])
+            secret_wrd = section_data[0]
+            prev_wrd = secret_wrd
+            for step, d in enumerate(section_data):
+                if step % 2 == 0 and step != 0:
+                    single_sim += self.calc_cos_sim(prev_wrd, d)
+                    if prev_wrd == d:
+                        success_rate += 1
+                    prev_wrd = d
+        
+        success_rate /= iteration * (step_num // 2)
+        single_sim /= iteration * (step_num // 2)
+        final_sim /= iteration
+        sim_ratio = single_sim / final_sim
+
+        return success_rate, single_sim, final_sim, sim_ratio
 
 
     def load_data(self, data_path, truncation, noise_degree):
@@ -77,8 +130,9 @@ if __name__ == "__main__":
     truncations = [0.004, 0.5, 1.0, 1.5, 2.0]
     noise_degrees = [0, 0.5, 1.0, 1.5, 2.0, 2.5, 3.0]
 
-    data_path = "../data/20220917"
+    data_path = "../data/20220918"
 
     analyzer = Analyzer(truncations, noise_degrees)
-    analyzer.analysis_manage(data_path)
+    #analyzer.analysis_manage(data_path)
+    analyzer.make_heatmap(data_path)
     
